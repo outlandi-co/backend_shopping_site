8ccf52c143554adc3ac842661bab13db125c04b9
outlandi-co <outlandico@gmail.com>
2024-07-21 02:29:28 -0700

    successful file upload

---
+++ src/routes/uploadRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { uploadFile } from '../controllers/uploadController.js';
+import upload from '../controllers/uploadController.js';
+
+const router = express.Router();
+
+router.post('/upload', upload.single('file'), uploadFile);
+
+export default router;
